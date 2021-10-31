f48b7df6c84f5bcbab3fbbaa9f160347f5d5b06f
Hamza Ensar <hamza_ensar123@outlook.com>
2021-10-31 18:22:32 +0100

    first commit

---
+++ list append range.py
@@ -0,0 +1,10 @@
+list1=[]
+list2=[]
+for i in range(2,13):
+    i= 1+i
+    list1.append(i)
+    print('list1 = ',list1)
+for z in range(-1,10):
+     z=1+z
+     list2.append(z)
+     print('list2: ',list2)
---
+++ list=[1,2,3,4,5].py
@@ -0,0 +1,6 @@
+list=[1,2,3,4,5]
+print('The normal list:',list)
+list.reverse()
+print("Reversed list",list)
+max_value=max(list)
+print("Maximum value is :" ,max(list),max_value-1)
